e3e189554ce363a22ccc5cf9a21ab5dc80f2855e
BrunoPalmucci <brunoapalmucci@sanluis.edu.ar>
2024-04-21 10:35:01 -0300

    Update server.js
    Agregada la traduccion

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ app.use(cors());
 app.get("/", async (req, res) => {
   try {
     const productos = await fetchProductos();
-    productos.forEach((producto) => {
-      producto.title = traducir(producto.title);
-      producto.description = traducir(producto.description);
+    const promises = productos.map(async (producto) => {
+      const tituloTraducido = await traducir(producto.title);
+      const descripcionTraducido = await traducir(producto.description);
+
+      producto.title = tituloTraducido.translation;
+      producto.description = descripcionTraducido.translation;
+
+      return producto;
     });
+
+    await Promise.all(promises);
     res.json({
       productos: productos,
     });
@@ -33,18 +40,25 @@ async function fetchProductos() {
   }
 }
 
-function traducir(texto) {
-  translate(
-    {
-      text: texto,
-      source: "en",
-      target: "es",
-    },
-    function (result) {
-      return result;
-    }
-  );
+async function traducir(texto) {
+  return new Promise((resolve, reject) => {
+    translate(
+      {
+        text: texto,
+        source: "en",
+        target: "es",
+      },
+      function (result) {
+        if (result && result.translation) {
+          resolve({ translation: result.translation });
+        } else {
+          reject("Error en la traducción");
+        }
+      }
+    );
+  });
 }
+
 
 app.listen(3000, () => {
   console.log("Server ejecutando en el port 3000");
